6e68345dee9cbd2693f8a84b72b6b0c0f4530a83
ZephirSD <siegfieddallery@gmail.com>
2022-01-10 16:10:29 +0400

    Random + Stateless / Statefull

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,14 +10,27 @@ void main() {
           title: Text('Dédé'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DiceChange(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
-  int leftDiceNumber = 5;
+class DiceChange extends StatefulWidget {
+  const DiceChange({Key? key}) : super(key: key);
+
+  @override
+  _DiceChangeState createState() => _DiceChangeState();
+}
+
+class _DiceChangeState extends State<DiceChange> {
+  int random1 = Random().nextInt(6) + 1;
+  void DiceChange1() {
+    setState(() {
+      random1 = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -27,11 +41,9 @@ class DicePage extends StatelessWidget {
             child: Padding(
               padding: EdgeInsets.all(16),
               child: FlatButton(
-                onPressed: () {
-                  print('Vous avez appuyé sur l\'image de gauche');
-                },
+                onPressed: (DiceChange1),
                 child: Image.asset(
-                  'images/dice$leftDiceNumber.png',
+                  'images/dice$random1.png',
                 ),
               ),
             ),
